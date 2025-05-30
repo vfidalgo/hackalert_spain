a288447f5bbe62d9be4e195c7b45013263c699ba
vfidalgo <dongash@gmail.com>
2025-05-30 23:15:36 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import { createRequire } from 'module'
+const require = createRequire(import.meta.url)
+
 
 export default defineConfig({
   plugins: [react()],
