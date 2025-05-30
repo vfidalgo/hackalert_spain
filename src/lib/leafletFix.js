45838655fc118cd16c182498e242cadebcae490b
vfidalgo <dongash@gmail.com>
2025-05-30 23:30:10 +0200

    Create leafletFix.js

---
+++ src/lib/leafletFix.js
@@ -0,0 +1,10 @@
+// Solución para errores de iconos en Leaflet
+import L from 'leaflet';
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
+  iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+});
