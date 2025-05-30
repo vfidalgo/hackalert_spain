b3a5e41887e2b0daca2301fd211e3f8c0ae79107
vfidalgo <dongash@gmail.com>
2025-05-30 23:30:48 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import './lib/leafletFix' // Importa la solución primero
 import App from './App'
 import './index.css' // Importa el CSS aquí
 
